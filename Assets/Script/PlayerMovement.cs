71e44b92fd115496850b3e5f67fa10a6278e2bfc
NakaVostro <ra00297288@pucsp.edu.br>
2023-09-27 18:57:01 -0300

    Player Movement & Jump Refine + Wall Slide & Wall Jump Start

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
@@ -22,7 +20,24 @@ public class PlayerMovement : MonoBehaviour {
 
     [Space(16)]
 
-    [SerializeField] private float _jumpForce; //
+    [SerializeField] private float _jumpHeight;
+
+    [Space(16)]
+
+    [SerializeField] private Vector2 _groundcheckOffset;
+    [SerializeField] private Vector2 _groundcheckBox;
+    [SerializeField] private LayerMask _groundcheckLayer;
+
+    [Header("Wall Slide")]
+
+    [SerializeField] private float _wallslideGravityScale;
+    [SerializeField] private Vector2 _wallslideOffset;
+    [SerializeField] private Vector2 _wallslideBox;
+    [SerializeField] private LayerMask _wallslideLayer;
+
+    [Header("Cache")]
+
+    private Vector2 _walljumpVector = Vector2.one.normalized;
 
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();
@@ -35,16 +50,16 @@ public class PlayerMovement : MonoBehaviour {
     private void FixedUpdate() {
         HorizontalMovement();
         Jump();
+        WallSlide();
+        WallJump();
     }
 
-    // Remember teach cooler way
     private void GetInput() {
         // Horizontal Movement
-        int i = 0;
-        if(Input.GetKey(_movementLeftKey)) i--;
-        if(Input.GetKey(_movementRightKey)) i++;
-        _inputMovement = i;
+        _inputMovement = (Input.GetKey(_movementLeftKey) ? -1 : 0) + 
+                         (Input.GetKey(_movementRightKey) ? 1 : 0);
 
+        // Jump Trigger
         if (Input.GetKeyDown(_jumpKey)) _inputJump = true;
     }
 
@@ -52,14 +67,48 @@ public class PlayerMovement : MonoBehaviour {
         _rb.velocity = new Vector2(_inputMovement * _movementSpeed, _rb.velocity.y);
     }
 
-    // Teach Jump based on height
     private void Jump() {
-        if (_inputJump) {
+        if (_inputJump && IsGrounded()) {
             _inputJump = false;
 
-            //_rb.AddForce(new Vector2(0, _jumpForce));//
-            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+            _rb.velocity = new Vector2(_rb.velocity.x, Mathf.Sqrt(2 * -Physics2D.gravity.y * _jumpHeight));
         }
     }
+
+    private bool IsGrounded() {
+        return Physics2D.OverlapBox((Vector2) transform.position + _groundcheckOffset, _groundcheckBox, 0, _groundcheckLayer);
+    }
+
+    private void WallSlide() {
+        _rb.gravityScale = IsSlidingWall(_inputMovement) ? _wallslideGravityScale : 1;
+    }
+
+    private void WallJump() {
+        if (_inputJump && IsSlidingWall(_inputMovement)) {
+            _inputJump = false;
+
+            _rb.velocity = (_inputMovement > 0 ? -_walljumpVector : _walljumpVector) * _jumpHeight; // Maybe specific force
+        }
+    }
+
+    private bool IsSlidingWall(int direction) {
+        if (direction == 0 || _rb.velocity.y > 0) return false;
+
+        return Physics2D.OverlapBox((Vector2)transform.position + (direction > 0 ? _wallslideOffset : -_wallslideOffset), _wallslideBox, 0, _wallslideLayer);
+    }
+
+#if UNITY_EDITOR
+    [Header("Debug")]
+
+    [SerializeField] private bool _showDebugGizmos;
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = IsGrounded() ? Color.green : Color.red;
+        Gizmos.DrawWireCube((Vector2)transform.position + _groundcheckOffset, _groundcheckBox);
+
+        Gizmos.color = IsSlidingWall(_inputMovement) ? Color.green : Color.red;
+        if(_inputMovement != 0) Gizmos.DrawWireCube((Vector2)transform.position + (_inputMovement > 0 ? _wallslideOffset : -_wallslideOffset), _wallslideBox);
+    }
+#endif
 
 }
